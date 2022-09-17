4baaf25ac873f003a8df83689fae604c4c21aad4
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-09-17 15:57:22 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//int fattoriale(int numero)
+//{
+//    int risultato = 0;
+
+//    if (numero > 1)
+//    {
+//        risultato = fattoriale(numero - 1) * numero ;
+//    }
+//    else
+//    {
+//        risultato = 1;
+//    }
+//    return risultato;
+//}
+
+//Console.WriteLine(fattoriale(3));
